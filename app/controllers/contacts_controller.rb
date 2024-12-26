a4a893ab5fd2ced0b14ebc6d40386d3af002fe32
Justogarcía <justo@uc.cl>
2024-12-26 12:20:07 -0300

    legumbre

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,8 @@ class ContactsController < ApplicationController
     if @contact.save
       redirect_to new_contact_path, notice: 'Mensaje enviado con éxito.'
     else
-      render :new, alert: 'Hubo un error al enviar el mensaje.'
+      flash.now[:alert] = 'Hubo un error al enviar el mensaje.'
+      render :new
     end
   end
 
